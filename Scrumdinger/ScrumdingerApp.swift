d58ea18ac365f25c7c86d69feda5f61dae3cc89e
Jay Khunt <jaykhunt741@gmail.com>
2022-05-05 15:04:47 +0530

    Initial commit - Scrumdinger

--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -9,9 +9,27 @@ import SwiftUI
 
 @main
 struct ScrumdingerApp: App {
+    @StateObject private var store = ScrumStore()
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                ScrumsView(scrums: $store.scrums){
+                    ScrumStore.save(scrums: store.scrums) { result in
+                        if case .failure(let error) = result {
+                            fatalError(error.localizedDescription)
+                        }
+                    }
+                }
+            }
+            .onAppear() {
+                ScrumStore.load { result in
+                    switch result {
+                    case .success(let scrums): store.scrums = scrums
+                    case .failure(let error): fatalError(error.localizedDescription)
+                    }
+                }
+            }
         }
     }
 }
